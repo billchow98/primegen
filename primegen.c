26013aee9b9195ad8931e548009f280608104d9b
billchow98 <billchow98@gmail.com>
2020-04-26 18:37:23 +0900

    Update primegen.c

--- primegen.c
+++ primegen.c
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2020 Bill Chow
+// Generates primes up to (2 ^ 32) - 1.
+// Stop when satisfied.
 
 #include <stdio.h>
-#include <limits.h>
 main() {
-    register i, a = 1;
-    start:
-    for (i = 2, ++a; i * i <= a; ++i) {
-        if (a == INT_MAX) return 0;
-        if (a % i == 0) goto start;
-    }
-    printf("%d ", a);
-    goto start;
+  register i, a = 1;
+start:
+  for (i = 2, ++a; i * i <= a; ++i) if (a % i == 0) goto start;
+  printf("%d ", a);
+  goto start;
 }
